9df8030b1a9e117379ba95d0455406ef7a026ad2
Aaron Conklin <conklin.aaron@gmail.com>
2017-06-29 11:39:53 -0400

    with photo links

--- scripts.js
+++ scripts.js
@@ -6,4 +6,10 @@ $(function() {
     $("html, body").animate({ scrollTop: 0}, "slow");
   });
 
+  $("#press").click(function(event) {
+    $(".press-photos").css('margin-top', '100px');
+    $(".press-img").css('height', '300px');
+    $(".bottom").css('height', '260px');
+  });
+
 });
